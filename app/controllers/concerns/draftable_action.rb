d638796fd51d4461717b75cd2b5c4daf7a4f32f3
suzuki_mar <supasu145@gmail.com>
2019-04-02 10:40:15 +0900

    draftable_save_action_serviceを削除した
    共通処理はconcernsに定義する

--- app/controllers/concerns/draftable_action.rb
+++ app/controllers/concerns/draftable_action.rb
@@ -2,10 +2,6 @@ module DraftableAction
 
   extend ActiveSupport::Concern
 
-  def draft_save_param?
-    params[:is_draft] == "true"
-  end
-
   def init_action(model_class)
     model = model_class.new
     raise NotImplementedError.new("initialize!を実装してください") unless model.respond_to?(:initialize!)
@@ -14,11 +10,39 @@ module DraftableAction
     render_success_with(model)
   end
 
-  def auto_save_action?
-    params[:action] == 'auto_save'
+  def draftable_save_action(model)
+    error_response = create_error_response_of_invalid_draftable_save_action_params
+    if error_response.present?
+      render_with_error_response(error_response)
+      return
+    end
+
+    raise NotImplementedError.new("create_save_paramsを実装してください") unless self.respond_to?(:create_save_params, true)
+    begin
+      if draft_save?(model)
+        model.save_draft!(create_save_params)
+      else
+        model.save_complete!(create_save_params)
+      end
+    rescue => e
+      error_response = create_error_response_of_save_failer(e, model)
+      render_with_error_response(error_response)
+      return
+    end
+
+    render_success_with(model)
   end
 
-  def create_error_response_of_draftable_save_action
+  private
+  def draft_save?(model)
+    auto_save_action? ? model.is_draft : (params[:is_draft] == "true")
+  end
+
+  def auto_save_action?
+    (params[:action] == 'auto_save')
+  end
+
+  def create_error_response_of_invalid_draftable_save_action_params
     if params[:is_draft].nil? && !auto_save_action?
       return ErrorResponse.create_validate_error_from_messages({is_draft: "必須です"})
     end
@@ -32,27 +56,12 @@ module DraftableAction
     nil
   end
 
-  def draftable_save_action(model)
+  def create_error_response_of_save_failer(error, model)
+    top_key_name = model.class.to_s.underscore.to_sym
+    error_messages = {}
+    error_messages[top_key_name] = error.message
 
-    error_response = create_error_response_of_draftable_save_action
-    if error_response.present?
-      render_with_error_response(error_response)
-      return
-    end
-
-    is_draft_save = auto_save_action? ? model.is_draft : draft_save_param?
-
-    raise NotImplementedError.new("create_save_paramsを実装してください") unless self.respond_to?(:create_save_params, true)
-    save_service = DraftableSaveService.new(is_draft_save, model, create_save_params)
-
-    if save_service.execute
-      render_success_with(model)
-    else
-      error_response = ErrorResponse.create_validate_error_from_messages(save_service.error_messages)
-      render_with_error_response(error_response)
-    end
-
+    error_response = ErrorResponse.create_validate_error_from_messages(error_messages)
   end
-
 
 end
