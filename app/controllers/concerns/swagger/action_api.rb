e91bb6207ec04b53a1bfc5f2506e879ea27cdc70
suzuki_mar <supasu145@gmail.com>
2019-04-12 13:24:09 +0900

    actions/done#GETのAPIドキュメントがなかった

--- app/controllers/concerns/swagger/action_api.rb
+++ app/controllers/concerns/swagger/action_api.rb
@@ -16,6 +16,35 @@ module Swagger::ActionApi
             'ver3'
         ]
 
+        response 200 do
+          key :description, '進行中のアクション一覧を取得する'
+          schema do
+            key :required, [:actions]
+
+            property :reframings do
+              key :type, :array
+              items do
+                key :'$ref', :ActionWithDocumentOutput
+              end
+            end
+
+          end
+        end
+
+      end
+
+    end
+
+    swagger_path '/actions/done' do
+
+      operation :get do
+        key :description, '進行中のアクション一覧を取得する'
+        key :operationId, :find_doing_actions
+
+        key :tags, [
+            'action',
+            'ver3'
+        ]
 
         response 200 do
           key :description, '進行中のアクション一覧を取得する'
